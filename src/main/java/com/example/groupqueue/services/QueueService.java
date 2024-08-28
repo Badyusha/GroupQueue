2f081081da6b24157b00e43051bb2c820af69352
Pablo Badzei <pashab200409@gmail.com>
2024-08-28 13:25:16 +0300

    docker

--- src/main/java/com/example/groupqueue/services/QueueService.java
+++ src/main/java/com/example/groupqueue/services/QueueService.java
@@ -16,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
